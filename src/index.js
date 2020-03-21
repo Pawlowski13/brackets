ce6d2a4b881f10364652abeedc878d8cefd9f21c
Marcin <pawlowskimarcin13@gmail.com>
2020-03-21 20:30:42 +0100

    submit solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let stack = []
+
+    bracketsConfig.map(el => {stack.push(el.join(""))});
+
+    for(i = 0; i <= stack.length; i++) {
+        stack.map(bracket => {
+        while(str.includes(bracket)){ str = str.replace(bracket, "")}})
+    }
+    
+    return (str.length === 0) ? true : false;
+
 }
